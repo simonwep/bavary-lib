c02aaacb618d824e84d2b698438019d0517b1200
Simon Reinisch <simon@reinisch.io>
2019-12-07 16:28:26 +0100

    Add custom ts-config for build

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,13 @@ module.exports = {
                 test: /\.(js|ts)$/,
                 include: path.resolve(__dirname, 'src'),
                 use: [
-                    'ts-loader',
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            context: __dirname,
+                            configFile: path.resolve('./tsconfig.build.json')
+                        }
+                    },
                     'eslint-loader'
                 ]
             }
